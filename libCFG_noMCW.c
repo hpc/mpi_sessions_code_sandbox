2f956a27cd7c4565a9036ad926acf62d90c3cb90
Howard Pritchard <hppritcha@gmail.com>
2018-05-08 07:57:11 -0600

    use publish/lookup to propagate port
    we need to use the MPI_Publish_name/lookup_name
    to transmit port info from the rank(s) doing the
    accepts.
    
    Code still hangs owing to a pmix bug.
    
    Signed-off-by: Howard Pritchard <hppritcha@gmail.com>

--- libCFG_noMCW.c
+++ libCFG_noMCW.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-int MPIX_COMM_CREATE_FROM_GROUP(MPI_Group group, int tag, MPI_Comm *comm) {
+int my_wrank;
+
+int MPIX_COMM_CREATE_FROM_GROUP(MPI_Group group, const char *tag, MPI_Comm *comm) {
     int groupRank, groupSize;
+    int zero = 0, localRank;
+    int localSize;
+    char port[MPI_MAX_PORT_NAME];
     MPI_Group localGroup;
     MPI_Group_rank(group, &groupRank);
     MPI_Group_size(group, &groupSize);
@@ -17,24 +22,34 @@ int MPIX_COMM_CREATE_FROM_GROUP(MPI_Group group, int tag, MPI_Comm *comm) {
         *comm = localComm;
         return MPI_SUCCESS;
     }
-    int localSize;
     MPI_Comm_size(localComm, &localSize);
     if (localSize < groupSize) {
-        char port[MPI_MAX_PORT_NAME];
-        MPI_Open_port(MPI_INFO_NULL, &port[0]);
+        MPI_Comm_group(localComm, &localGroup);
+        MPI_Group_translate_ranks(group, 1, &zero, localGroup, &localRank);
+        if (MPI_UNDEFINED == localRank) {
+               sleep(5); /* temporary till we do better error code return values */
+               MPI_Lookup_name(tag, MPI_INFO_NULL, port);
+        } else {
+                MPI_Open_port(MPI_INFO_NULL, &port[0]);
+                MPI_Publish_name(tag, MPI_INFO_NULL, port);
+        }
         while (localSize < groupSize) {
             MPI_Comm_group(localComm, &localGroup);
-            int zero = 0, localRank;
             MPI_Group_translate_ranks(group, 1, &zero, localGroup, &localRank);
             MPI_Comm interComm;
             if (MPI_UNDEFINED == localRank) {
+                fprintf(stderr, "rank %d connecting to port %s\n", my_wrank, port);
                 MPI_Comm_connect(port, MPI_INFO_NULL, 0, localComm, &interComm);
                 MPI_Comm_free(&localComm);
+                fprintf(stderr, "rank %d merging intercomm \n", my_wrank);
                 MPI_Intercomm_merge(interComm, 1, &localComm);
                 MPI_Comm_disconnect(&interComm);
             } else {
+                fprintf(stderr, "rank %d accepting on port %s\n", my_wrank, port);
+                MPI_Open_port(MPI_INFO_NULL, &port[0]);
                 MPI_Comm_accept(port, MPI_INFO_NULL, 0, localComm, &interComm);
                 MPI_Comm_free(&localComm);
+                fprintf(stderr, "rank %d merging intercomm \n", my_wrank);
                 MPI_Intercomm_merge(interComm, 0, &localComm);
                 MPI_Comm_disconnect(&interComm);
             }
@@ -48,7 +63,7 @@ int MPIX_COMM_CREATE_FROM_GROUP(MPI_Group group, int tag, MPI_Comm *comm) {
 
 int main(int argc, char **argv)
 {
-    int i, n,  my_wrank, wsize, *granks = NULL;
+    int i, n,  wsize, *granks = NULL;
     MPI_Group wgroup, newgroup;
     MPI_Comm new_comm;
     MPI_Init(&argc, &argv);
@@ -65,7 +80,7 @@ int main(int argc, char **argv)
     MPI_Group_incl(wgroup, (wsize + 1)/2, granks, &newgroup);
     if (my_wrank % 2 == 0) {
         fprintf(stderr, "process %d (MPI_COMM_WORLD) calling from group thingy\n", my_wrank);
-        MPIX_COMM_CREATE_FROM_GROUP(newgroup, 10, &new_comm);
+        MPIX_COMM_CREATE_FROM_GROUP(newgroup, "foobar10", &new_comm);
         fprintf(stderr, "process %d (MPI_COMM_WORLD) now calling barrier \n",
                 my_wrank);
         MPI_Barrier(new_comm);
